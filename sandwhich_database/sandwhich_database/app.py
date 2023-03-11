09b4d0d1ffb8dfdea7a4204e88f28f9c4536de9f
student <student@provisions-MacBook-Air-10.local>
2023-03-11 16:06:50 -0500

    message 1

--- sandwhich_database/sandwhich_database/app.py
+++ sandwhich_database/sandwhich_database/app.py
@@ -6,10 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    sandwich = get_all_sandwich()
 
-    return render_template('index.html')
+    return render_template('index.html', sandwich = sandwich)
 
 
 if __name__== '__main__':
     app.run(debug=True, host='0.0.0.0')
 
+def edit(rowid):
+    sandwich = get_sandwich
+
+
+
+
+#(name, bread, cheese, meat, sauce, toping, anything)
